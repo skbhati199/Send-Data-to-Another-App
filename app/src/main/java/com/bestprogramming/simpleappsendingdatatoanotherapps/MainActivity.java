cebd23f37acf96ebc9a25090d101d5524ac4d2aa
amitgroch <amitgrochdennis@yahoo.com>
2017-04-25 15:33:44 +0530

    Integrated Firebase done

--- app/src/main/java/com/bestprogramming/simpleappsendingdatatoanotherapps/MainActivity.java
+++ app/src/main/java/com/bestprogramming/simpleappsendingdatatoanotherapps/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,26 +13,95 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.crash.FirebaseCrash;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int PICK_FROM_GALLERY = 101;
+    private static final String TAG = MainActivity.class.getSimpleName();
     EditText editTextEmail, editTextSubject, editTextMessage;
     Button btnSend, btnAttachment;
     String email, subject, message, attachmentFile;
     Uri URI = null;
     int columnIndex;
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    private StorageReference mStorageRef;
+    private Button btnUpload;
+    private StorageReference riversRef;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        FirebaseCrash.log("Activity created");
         editTextEmail = (EditText) findViewById(R.id.editTextTo);
         editTextSubject = (EditText) findViewById(R.id.editTextSubject);
         editTextMessage = (EditText) findViewById(R.id.editTextMessage);
         btnAttachment = (Button) findViewById(R.id.buttonAttachment);
         btnSend = (Button) findViewById(R.id.buttonSend);
+        btnUpload = (Button) findViewById(R.id.upload);
+
+        btnUpload.setOnClickListener(this);
 
         btnSend.setOnClickListener(this);
         btnAttachment.setOnClickListener(this);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "1");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, MainActivity.class.getSimpleName());
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+
+
+//        Real Time DataBase add
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -52,42 +122,108 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    public void uploadImage(){
+//        Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
+        riversRef = mStorageRef.child("images/rivers.jpg");
+
+        riversRef.putFile(URI)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Toast.makeText(MainActivity.this,"onSuccess Uploaded", Toast.LENGTH_LONG).show();
+//                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                        Toast.makeText(MainActivity.this,"onFailure Uploaded", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+    }
+
     @Override
     public void onClick(View v) {
 
-        if (v == btnAttachment) {
-            openGallery();
+        switch (v.getId()){
+            case R.id.upload:
+                uploadImage();
+                break;
+            case R.id.buttonAttachment:
+                openGallery();
+                break;
+            case R.id.buttonSend:
+                try {
 
-        }
-        if (v == btnSend) {
-            try {
-                email = editTextEmail.getText().toString();
-                subject = editTextSubject.getText().toString();
-                message = editTextMessage.getText().toString();
+                    email = editTextEmail.getText().toString();
+                    subject = editTextSubject.getText().toString();
+                    message = editTextMessage.getText().toString();
 
-                final Intent emailIntent = new Intent(
-                        Intent.ACTION_SEND);
-                
-                emailIntent.setType("image/jpeg");
+                    final Intent emailIntent = new Intent(
+                            Intent.ACTION_SEND);
+
+                    emailIntent.setType("image/jpeg");
                /* emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
                         new String[] { email });*/
                 /*emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
                         subject);*/
-                if (URI != null) {
-                    emailIntent.putExtra(Intent.EXTRA_STREAM, URI);
-                }
+                    if (URI != null) {
+                        emailIntent.putExtra(Intent.EXTRA_STREAM, URI);
+                    }
                 /*emailIntent
                         .putExtra(android.content.Intent.EXTRA_TEXT, message);
                 */this.startActivity(Intent.createChooser(emailIntent,
-                        "Sending email..."));
+                            "Sending email..."));
 
-            } catch (Throwable t) {
-                Toast.makeText(this,
-                        "Request failed try again: " + t.toString(),
-                        Toast.LENGTH_LONG).show();
-            }
+                } catch (Throwable t) {
+                    Toast.makeText(this,
+                            "Request failed try again: " + t.toString(),
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.download:
+                downloadFile();
+                break;
         }
 
+
+
+    }
+
+
+    private void downloadFile(){
+        File localFile = null;
+        try {
+            localFile = File.createTempFile("images/rivers.jpg", "jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (localFile != null) {
+            riversRef.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        long bytes;
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            // Successfully downloaded data to local file
+                            // ...
+                             bytes =taskSnapshot.getBytesTransferred();
+                            taskSnapshot.getTotalByteCount();
+                            Toast.makeText(MainActivity.this, "Download File" + " BytesTransferred " + bytes +
+                                    " Total Bytes : " + taskSnapshot.getTotalByteCount(), Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle failed download
+                    // ...
+                    Toast.makeText(MainActivity.this, "onFailure Download", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 
     public void openGallery() {
